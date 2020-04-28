527c9f95925084fb2f99eb4ebcaad62c41d4c1ac
Gyuri Trum <gyurui@Gyuris-MacBook-Pro.local>
2020-04-28 22:37:13 +0200

    security vulnabirty fixed

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-  ID SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  title VARCHAR(255) NOT NULL
-);
-
-INSERT INTO users (name, title)
-VALUES  ('Test User', 'Student');
