b25b2fe346b7287fe9181a1c8f65823ad0c27cd4
Gyuri Trum <gyurui@Gyuris-MacBook-Pro.local>
2020-05-16 11:35:41 +0200

    test automatic deploy from github

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -16,7 +16,7 @@ export class Routes {
     public routes(app: Application): void {
         app.route("/").get((req: Request, res: Response) => {
             res.status(200).send({
-                message: "ok",
+                message: "Hello DMS",
             });
         });
 
